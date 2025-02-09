aef4d1aecd847799c40e6326154c8e288a9c5431
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2025-02-09 20:18:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Lavender-Region",
-    version="1.0.3",
+    version="1.0.4",
     author="Zeyu Xie",
     author_email="xie.zeyu20@gmail.com",
     description="A pip package for region data",
